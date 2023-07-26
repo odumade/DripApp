9acea4ef60f18fdbb3dfdf0061ed4931b96a7d89
odumade <chekkitapp@gmail.com>
2023-07-26 12:24:08 +0100

    complete app with docker set up

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -8,18 +8,24 @@ from core.user.serializers import UserSerializer
 
 
 class PostSerializer(AbstractSerializer):
-    author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
+    author = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field="public_id"
+    )
     liked = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
+    comments_count = serializers.SerializerMethodField()
+
+    def get_comments_count(self, instance):
+        return instance.comment_set.count()
 
     def get_liked(self, instance):
 
-        request = self.context.get('request', None)
+        request = self.context.get("request", None)
 
         if request is None or request.user.is_anonymous:
             return False
 
-        return request.user.has_liked(instance)
+        return request.user.has_liked_post(instance)
 
     def get_likes_count(self, instance):
         return instance.liked_by.count()
@@ -31,7 +37,7 @@ class PostSerializer(AbstractSerializer):
 
     def update(self, instance, validated_data):
         if not instance.edited:
-            validated_data['edited'] = True
+            validated_data["edited"] = True
 
         instance = super().update(instance, validated_data)
 
@@ -40,12 +46,22 @@ class PostSerializer(AbstractSerializer):
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = User.objects.get_object_by_public_id(rep["author"])
-        rep["author"] = UserSerializer(author).data
+        rep["author"] = UserSerializer(author, context=self.context).data
 
         return rep
 
     class Meta:
         model = Post
         # List of all the fields that can be included in a request or a response
-        fields = ['id', 'author', 'body', 'edited', 'liked', 'likes_count', 'created', 'updated']
+        fields = [
+            "id",
+            "author",
+            "body",
+            "edited",
+            "liked",
+            "likes_count",
+            "comments_count",
+            "created",
+            "updated",
+        ]
         read_only_fields = ["edited"]
